0296d8b4859505c4321407fb49e11f775c56796d
James Kelly <james+kellyj4@users.noreply.github.com>
2025-11-16 15:51:39 +1100

    Adding the extras

--- modules/ses/main.tf
+++ modules/ses/main.tf
@@ -5,6 +5,7 @@ resource "aws_cloudformation_stack" "ses_identity" {
         MailFromDomainName = var.mailfromdomain
         ExistingVPCID = var.vpc_id
         ExistingVPCSubnetIDs = join(",", var.vpc_subnet_ids)
+        ExistingSNSTopicARN = var.sns_topic_arn
     }
     template_body = file("./modules/ses/ses_email_identity.yml")
 }
